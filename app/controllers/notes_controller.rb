f26e24035ff4aa73a4d131b8b949ad0003f673fc
Stefan Petrov <stefanp227@gmail.com>
2016-04-27 15:53:01 +0300

    Add update, destroy, edit, create

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,19 +13,46 @@ class NotesController < ApplicationController
   end
 
   def create
-
+    @note = Note.new(note_params)
+    respond_to do |format|
+      if @note.save
+        format.json { render json: @note, status: :created }
+        format.xml { render xml: @note, status: :created }
+      else
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+        format.xml { render xml: @note.errors, status: :unprocessable_entity }
+      end
+    end
   end
-
+    
   def destroy
+    respond_to do |format|
+      if @note.destroy
+        format.json { head :no_content, status: :ok }
+        format.xml { head :no_content, status: :ok }
+      else
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+        format.xml { render xml: @note.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   end
 
   def edit
-
+    @note = Note.find(params[:id])
   end
 
   def update
-
+    respond_to do |format|
+      if @note.update(note_params)
+        format.json { head :no_content, status: :ok }
+        format.xml { head :no_content, status: :ok }
+      else
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+        format.xml { rednder xml: @note.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
