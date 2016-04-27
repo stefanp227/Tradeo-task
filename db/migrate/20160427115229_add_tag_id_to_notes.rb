c8c20d63e4e64022dd657eaead1029aa3400589a
Stefan Petrov <stefanp227@gmail.com>
2016-04-27 14:55:42 +0300

    Add tag_id to notes

---
+++ db/migrate/20160427115229_add_tag_id_to_notes.rb
@@ -0,0 +1,5 @@
+class AddTagIdToNotes < ActiveRecord::Migration[5.0]
+  def change
+  	add_column :notes, :tag_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427113829) do
+ActiveRecord::Schema.define(version: 20160427115229) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title",      null: false
     t.string   "content",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
