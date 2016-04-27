b46993054e39c108a717b61a33d88b00a16cf20d
Stefan Petrov <stefanp227@gmail.com>
2016-04-27 15:14:29 +0300

    Add index, new, show

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,37 @@
 class NotesController < ApplicationController
+
+  def index
+    @notes = Note.all
+  end
+
+	def new
+    @note = Note.new
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :content)
+  end
+
 end
